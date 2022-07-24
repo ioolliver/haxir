442bbc802f408eb623de21f3b30770c56dda454c
Pedro Henrique <pedrohfonseca.1981@gmail.com>
2022-07-24 18:56:02 -0300

    fix: formatting and removing else-if

--- priv/frontend/dist/index.js
+++ priv/frontend/dist/index.js
@@ -20,42 +20,42 @@ function startBrowser() {
         let browser = yield (0, puppeteer_1.launch)({
             headless: true,
             args: [
-                '--autoplay-policy=user-gesture-required',
-                '--disable-background-networking',
-                '--disable-background-timer-throttling',
-                '--disable-backgrounding-occluded-windows',
-                '--disable-breakpad',
-                '--disable-client-side-phishing-detection',
-                '--disable-component-update',
-                '--disable-default-apps',
-                '--disable-dev-shm-usage',
-                '--disable-domain-reliability',
-                '--disable-extensions',
-                '--disable-features=AudioServiceOutOfProcess',
-                '--disable-hang-monitor',
-                '--disable-ipc-flooding-protection',
-                '--disable-notifications',
-                '--disable-offer-store-unmasked-wallet-cards',
-                '--disable-popup-blocking',
-                '--disable-print-preview',
-                '--disable-prompt-on-repost',
-                '--disable-renderer-backgrounding',
-                '--disable-setuid-sandbox',
-                '--disable-speech-api',
-                '--disable-sync',
-                '--hide-scrollbars',
-                '--ignore-gpu-blacklist',
-                '--metrics-recording-only',
-                '--mute-audio',
-                '--no-default-browser-check',
-                '--no-first-run',
-                '--no-pings',
-                '--no-sandbox',
-                '--no-zygote',
-                '--password-store=basic',
-                '--use-gl=swiftshader',
-                '--use-mock-keychain',
-            ]
+                "--autoplay-policy=user-gesture-required",
+                "--disable-background-networking",
+                "--disable-background-timer-throttling",
+                "--disable-backgrounding-occluded-windows",
+                "--disable-breakpad",
+                "--disable-client-side-phishing-detection",
+                "--disable-component-update",
+                "--disable-default-apps",
+                "--disable-dev-shm-usage",
+                "--disable-domain-reliability",
+                "--disable-extensions",
+                "--disable-features=AudioServiceOutOfProcess",
+                "--disable-hang-monitor",
+                "--disable-ipc-flooding-protection",
+                "--disable-notifications",
+                "--disable-offer-store-unmasked-wallet-cards",
+                "--disable-popup-blocking",
+                "--disable-print-preview",
+                "--disable-prompt-on-repost",
+                "--disable-renderer-backgrounding",
+                "--disable-setuid-sandbox",
+                "--disable-speech-api",
+                "--disable-sync",
+                "--hide-scrollbars",
+                "--ignore-gpu-blacklist",
+                "--metrics-recording-only",
+                "--mute-audio",
+                "--no-default-browser-check",
+                "--no-first-run",
+                "--no-pings",
+                "--no-sandbox",
+                "--no-zygote",
+                "--password-store=basic",
+                "--use-gl=swiftshader",
+                "--use-mock-keychain",
+            ],
         });
         return browser;
     });
@@ -80,8 +80,10 @@ function managePage(browser) {
 }
 function startBot(page) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield page.evaluate(() => { window.exports = {}; });
-        (0, fs_1.readFile)(path_1.default.join(__dirname, "/bot.js"), 'utf-8', (err, data) => {
+        yield page.evaluate(() => {
+            window.exports = {};
+        });
+        (0, fs_1.readFile)(path_1.default.join(__dirname, "/bot.js"), "utf-8", (err, data) => {
             if (err)
                 return console.log("ERROR: ", err);
             page.evaluate(data);
--- priv/frontend/src/index.ts
+++ priv/frontend/src/index.ts
@@ -1,93 +1,87 @@
 import { Browser, launch, Page } from "puppeteer";
-import { readFile } from "fs"
+import { readFile } from "fs";
 import path from "path";
 
-async function startBrowser() : Promise<Browser> {
-
+async function startBrowser(): Promise<Browser> {
   let browser = await launch({
     headless: true,
     args: [
-        '--autoplay-policy=user-gesture-required',
-        '--disable-background-networking',
-        '--disable-background-timer-throttling',
-        '--disable-backgrounding-occluded-windows',
-        '--disable-breakpad',
-        '--disable-client-side-phishing-detection',
-        '--disable-component-update',
-        '--disable-default-apps',
-        '--disable-dev-shm-usage',
-        '--disable-domain-reliability',
-        '--disable-extensions',
-        '--disable-features=AudioServiceOutOfProcess',
-        '--disable-hang-monitor',
-        '--disable-ipc-flooding-protection',
-        '--disable-notifications',
-        '--disable-offer-store-unmasked-wallet-cards',
-        '--disable-popup-blocking',
-        '--disable-print-preview',
-        '--disable-prompt-on-repost',
-        '--disable-renderer-backgrounding',
-        '--disable-setuid-sandbox',
-        '--disable-speech-api',
-        '--disable-sync',
-        '--hide-scrollbars',
-        '--ignore-gpu-blacklist',
-        '--metrics-recording-only',
-        '--mute-audio',
-        '--no-default-browser-check',
-        '--no-first-run',
-        '--no-pings',
-        '--no-sandbox',
-        '--no-zygote',
-        '--password-store=basic',
-        '--use-gl=swiftshader',
-        '--use-mock-keychain',
-    ]
-  })
+      "--autoplay-policy=user-gesture-required",
+      "--disable-background-networking",
+      "--disable-background-timer-throttling",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-breakpad",
+      "--disable-client-side-phishing-detection",
+      "--disable-component-update",
+      "--disable-default-apps",
+      "--disable-dev-shm-usage",
+      "--disable-domain-reliability",
+      "--disable-extensions",
+      "--disable-features=AudioServiceOutOfProcess",
+      "--disable-hang-monitor",
+      "--disable-ipc-flooding-protection",
+      "--disable-notifications",
+      "--disable-offer-store-unmasked-wallet-cards",
+      "--disable-popup-blocking",
+      "--disable-print-preview",
+      "--disable-prompt-on-repost",
+      "--disable-renderer-backgrounding",
+      "--disable-setuid-sandbox",
+      "--disable-speech-api",
+      "--disable-sync",
+      "--hide-scrollbars",
+      "--ignore-gpu-blacklist",
+      "--metrics-recording-only",
+      "--mute-audio",
+      "--no-default-browser-check",
+      "--no-first-run",
+      "--no-pings",
+      "--no-sandbox",
+      "--no-zygote",
+      "--password-store=basic",
+      "--use-gl=swiftshader",
+      "--use-mock-keychain",
+    ],
+  });
 
-  return browser
-
+  return browser;
 }
 
-async function getFirstPage(browser : Browser) : Promise<Page> {
-  let page : Page
-  let pages = await browser.pages()
+async function getFirstPage(browser: Browser): Promise<Page> {
+  let page: Page;
+  let pages = await browser.pages();
 
-  if(pages.length > 0) page = pages[0]
-  else page = await browser.newPage()
+  if (pages.length > 0) page = pages[0];
+  else page = await browser.newPage();
 
-  return page
+  return page;
 }
 
-async function managePage(browser : Browser) : Promise<Page> {
+async function managePage(browser: Browser): Promise<Page> {
+  let page = await getFirstPage(browser);
 
-  let page = await getFirstPage(browser)
+  await page.goto("https://www.haxball.com/headless");
 
-  await page.goto("https://www.haxball.com/headless")
-
-  return page
-
+  return page;
 }
 
-async function startBot(page : Page) {
-  await page.evaluate(() => { window.exports = {} })
-  
-  readFile(path.join(__dirname, "/bot.js"), 'utf-8', (err, data) => {
+async function startBot(page: Page) {
+  await page.evaluate(() => {
+    window.exports = {};
+  });
 
-    if(err) return console.log("ERROR: ", err)
+  readFile(path.join(__dirname, "/bot.js"), "utf-8", (err, data) => {
+    if (err) return console.log("ERROR: ", err);
 
-    page.evaluate(data)
-
-  })
+    page.evaluate(data);
+  });
 }
 
 async function main() {
+  let browser = await startBrowser();
+  let page = await managePage(browser);
 
-  let browser = await startBrowser()
-  let page = await managePage(browser)
-
-  await startBot(page)
-  
+  await startBot(page);
 }
 
-main()
+main();
