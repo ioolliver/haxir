b4d553b4fb641e7619f2da6715798efeff327232
tyde81 <pedrohfonseca.1981@gmail.com>
2023-01-30 23:28:18 -0300

    fix: plugin system

--- lib/haxir/consumer.ex
+++ lib/haxir/consumer.ex
@@ -31,18 +31,24 @@ defmodule Haxir.Consumer do
       def handle_events(events, _from, state) do
         final_state =
           for event <- events do
-            plugins =
-              __metadata__().plugins |> Enum.map(fn {plugin} -> plugin end) |> Enum.reverse()
-
             plugins_state =
-              Enum.reduce(plugins, fn x, acc ->
-                {:state, plugin_state} = apply(x, :handle_event, [event, state])
+              case __metadata__().plugins do
+                [] ->
+                  %{}
 
-                {:state, plugin_state} =
-                  apply(acc, :handle_event, [event, Map.merge(state, plugin_state)])
+                plugins ->
+                  __metadata__().plugins
+                  |> Enum.map(fn {plugin} -> plugin end)
+                  |> Enum.reverse()
+                  |> Enum.reduce(fn x, acc ->
+                    {:state, plugin_state} = apply(x, :handle_event, [event, state])
 
-                plugin_state
-              end)
+                    {:state, plugin_state} =
+                      apply(acc, :handle_event, [event, Map.merge(state, plugin_state)])
+
+                    plugin_state
+                  end)
+              end
 
             {:state, new_state} = handle_event(event, Map.merge(state, plugins_state))
 
