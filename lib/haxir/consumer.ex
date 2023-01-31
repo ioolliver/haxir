5c90c465888b17c4e0bb3fe2423c115970e78afa
tyde81 <pedrohfonseca.1981@gmail.com>
2023-01-30 23:04:40 -0300

    feat: add plugin system

--- lib/haxir/consumer.ex
+++ lib/haxir/consumer.ex
@@ -1,10 +1,24 @@
 defmodule Haxir.Consumer do
   @moduledoc false
 
+  defmacro plugin(module) do
+    quote do
+      @plugins {unquote(module)}
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def __metadata__, do: %{plugins: @plugins}
+    end
+  end
+
   defmacro __using__(_) do
     quote do
-
       use GenStage
+      import unquote(__MODULE__)
+
+      Module.register_attribute(__MODULE__, :plugins, accumulate: true)
 
       def start_link(_initial) do
         GenStage.start_link(__MODULE__, :ok)
@@ -15,18 +29,37 @@ defmodule Haxir.Consumer do
       end
 
       def handle_events(events, _from, state) do
-        final_state = for event <- events do
-          {:state, new_state} = handle_event(event, state)
-          new_state
-        end
-        |> Enum.reduce(fn st, acc ->
-          if is_map(acc) do Map.merge(acc, st) else st end
-        end)
+        final_state =
+          for event <- events do
+            plugins =
+              __metadata__().plugins |> Enum.map(fn {plugin} -> plugin end) |> Enum.reverse()
+
+            plugins_state =
+              Enum.reduce(plugins, fn x, acc ->
+                {:state, plugin_state} = apply(x, :handle_event, [event, state])
+
+                {:state, plugin_state} =
+                  apply(acc, :handle_event, [event, Map.merge(state, plugin_state)])
+
+                plugin_state
+              end)
+
+            {:state, new_state} = handle_event(event, Map.merge(state, plugins_state))
+
+            new_state
+          end
+          |> Enum.reduce(fn st, acc ->
+            if is_map(acc) do
+              Map.merge(acc, st)
+            else
+              st
+            end
+          end)
 
         {:noreply, [], final_state}
       end
 
+      @before_compile unquote(__MODULE__)
     end
   end
-
 end
